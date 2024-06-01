f0471574acd5605ad68785092268764898d03560
andi <andy.tsl@pm.me>
2024-06-01 15:12:27 +0300

    symmetric adding firework sparkles

--- Assets/Scripts/Cells/FireworkUpdater.cs
+++ Assets/Scripts/Cells/FireworkUpdater.cs
@@ -12,7 +12,6 @@ public unsafe class FireworkUpdater : CellUpdater
     {
         int x = cellPtr->x;
         int y = cellPtr->y;
-        Debug.Log($"Generation: {cellPtr->generation}");
 
         for (int i = 0; i < 3; i++)
         {
@@ -24,16 +23,41 @@ public unsafe class FireworkUpdater : CellUpdater
             int radius = Random.Range(40, 100);
             int colorscale = Random.Range(0, 6);
 
-            simulation.TryAdd(x, y + 2, CellType.Sparkle, radius, colorscale, 0, 7);
-            simulation.TryAdd(x + 2, y + 2, CellType.Sparkle, radius, colorscale, 5, 5);
-            simulation.TryAdd(x + 2, y, CellType.Sparkle, radius, colorscale, 7, 0);
-            simulation.TryAdd(x + 2, y - 2, CellType.Sparkle, radius, colorscale, 5, -5);
-            simulation.TryAdd(x, y - 2, CellType.Sparkle, radius, colorscale, 0, -7);
-            simulation.TryAdd(x - 2, y - 2, CellType.Sparkle, radius, colorscale, -5, -5);
-            simulation.TryAdd(x - 2, y, CellType.Sparkle, radius, colorscale, -7, 0);
-            simulation.TryAdd(x - 2, y + 2, CellType.Sparkle, radius, colorscale, -5, 5);
+            AddSymmetric(x, y, dx: 0, dy: 2, vx: 0, vy: 7, radius, colorscale);
+            AddSymmetric(x, y, dx: 1, dy: 2, vx: 3, vy: 7, radius, colorscale);
+            AddSymmetric(x, y, dx: 2, dy: 2, vx: 5, vy: 5, radius, colorscale);
+            AddSymmetric(x, y, dx: 2, dy: 1, vx: 7, vy: 3, radius, colorscale);
+            AddSymmetric(x, y, dx: 2, dy: 0, vx: 7, vy: 0, radius, colorscale);
+
             simulation.Remove(x, y);
             return;
+        }
+
+    }
+
+    private void AddSymmetric(
+        int x, int y,
+        int dx, int dy,
+        int vx, int vy,
+        int generation, int colorscale)
+    {
+        if (dx != 0 && dy != 0)
+        {
+            simulation.TryAdd(x + dx, y + dy, CellType.Sparkle, generation, colorscale, vx, vy);
+            simulation.TryAdd(x + dx, y - dy, CellType.Sparkle, generation, colorscale, vx, -vy);
+
+            simulation.TryAdd(x - dx, y + dy, CellType.Sparkle, generation, colorscale, -vx, vy);
+            simulation.TryAdd(x - dx, y - dy, CellType.Sparkle, generation, colorscale, -vx, -vy);
+        }
+        else if (dx == 0)
+        {
+            simulation.TryAdd(x, y + dy, CellType.Sparkle, generation, colorscale, vx, vy);
+            simulation.TryAdd(x, y - dy, CellType.Sparkle, generation, colorscale, vx, -vy);
+        }
+        else
+        {
+            simulation.TryAdd(x + dx, y, CellType.Sparkle, generation, colorscale, vx, vy);
+            simulation.TryAdd(x - dx, y, CellType.Sparkle, generation, colorscale, -vx, vy);
         }
     }
 }
